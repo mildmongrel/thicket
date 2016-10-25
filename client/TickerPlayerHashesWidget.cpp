5fd76f3cd32db837198f4249ca8b59552937003a
mildmongrel <mildmongrel@gmail.com>
2016-10-25 08:11:14 -0700

    Show hashes in ticker only when available

--- client/TickerPlayerHashesWidget.cpp
+++ client/TickerPlayerHashesWidget.cpp
@@ -12,36 +12,32 @@ TickerPlayerHashesWidget::update( const RoomStateAccumulator& roomState )
 {
     qtutils::clearLayout( mLayout );
 
+    int hashes = 0;
     for( int i = 0; i < roomState.getChairCount(); ++i )
     {
-        QLabel* label;
-
         if( roomState.hasPlayerName( i ) && roomState.hasPlayerCockatriceHash( i ) )
         {
             QString name = QString::fromStdString( roomState.getPlayerName( i ) );
             QString hash = QString::fromStdString( roomState.getPlayerCockatriceHash( i ) );
-            label = new QLabel( QString( "<b>%1:</b> %2" ).arg( name ).arg( hash ) );
-        }
-        else
-        {
-            label = new QLabel( "(open chair)" );
-            label->setStyleSheet( "QLabel { color: gray; }" );
-        }
+            QLabel* label = new QLabel( QString( "<b>%1:</b> %2" ).arg( name ).arg( hash ) );
 
-        label->setContentsMargins( 0, 0, 0, 0 );
-        mLayout->addWidget( label );
+            label->setContentsMargins( 0, 0, 0, 0 );
 
-        if( i < roomState.getChairCount() - 1 )
-        {
-            mLayout->addSpacing( 30 );
-        }
+            if( hashes > 0 )
+            {
+                mLayout->addSpacing( 30 );
+            }
+            mLayout->addWidget( label );
 
-        // It's possible (likely?) that this widget is showing.  If so, Qt
-        // doesn't automatically show new widgets when they're added to a layout.
-        // Need to manually make them visible.
-        if( !isHidden() )
-        {
-            label->setVisible(true);
+            // It's possible (likely?) that this widget is showing.  If so, Qt
+            // doesn't automatically show new widgets when they're added to a layout.
+            // Need to manually make them visible.
+            if( !isHidden() )
+            {
+                label->setVisible(true);
+            }
+
+            hashes++;
         }
     }
 
